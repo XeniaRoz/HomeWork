125789244c2cdabfd02c7e935deadd2e385824e1
Ksenia Rozenbaum <kapellkaa@inbox.ru>
2023-03-26 23:03:43 +0700

    Домашняя работа "Доктор - пациент"

---
+++ DZ_Clinica/Doctor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Clinica
+{
+	public class Doctor : Person
+	{
+		private int Stage;
+		private string Speciality;
+
+		public Doctor(string pName, string pSurname, int pAge, int pStage, string pSpeciality) : base(pName, pSurname, pAge)
+		{
+			SetSpeciality(pSpeciality);
+			SetStage(pStage);
+		}
+
+		public string GetSpeciality() { return Speciality; }
+		public int GetStage() { return Stage; }
+
+		private void SetSpeciality(string pSpeciality) { Speciality = pSpeciality; }
+		private void SetStage(int pStage) { Stage = pStage; }
+
+		public void Cure(Patient bolnoy)
+		{
+			switch(bolnoy.GetStatus())
+			{
+				case "Болен":
+                    {
+						Console.WriteLine("Пациент действительно болен. Нажмите клавишу Enter, чтобы вылечить");
+						bolnoy.SetBolezn("");
+						Console.ReadLine();
+						Console.WriteLine("Пациент вылечился!");
+					}; break;
+				case "Здоров":
+                    {
+						Console.WriteLine("Пациент уже здоров!"); break;
+                    }
+				default: Console.WriteLine("Не понятно, что с пациентом"); break;
+			}
+		} 
+	}
+}
---
+++ DZ_Clinica/Patient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Clinica
+{
+	public class Patient : Person
+	{
+		private string Bolezn;
+		private  string Adress;
+
+		public Patient(string pName, string pSurname, int pAge, string pAdress, string pBolezn) : base(pName, pSurname, pAge)
+		{
+			SetBolezn(pBolezn);
+			SetAdress(pAdress);
+		}
+
+		public string GetBolezn() { return Bolezn; }
+		public string GetAdress() { return Adress; }
+
+		public void SetBolezn(string pBolezn) { 	Bolezn = pBolezn; }
+		private void SetAdress(string pAdress) { Adress = pAdress; }
+		public string GetStatus()
+        {
+			if (Bolezn == "")
+				return "Здоров";
+			else
+				return "Болен";
+        }
+
+	}
+}
