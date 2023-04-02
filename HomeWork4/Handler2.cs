fa0a005745de578990691589186d8291bbe4dc49
XeniaRoz <127036335+XeniaRoz@users.noreply.github.com>
2023-04-03 00:00:54 +0700

    Revert "Revert "Домашняя работа "события и делегаты"""

---
+++ HomeWork4/Handler2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    public class Handler2
+    {
+        public Handler2()
+        {
+
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("Ура, досчитал до 42!");
+        }
+    }
+}
