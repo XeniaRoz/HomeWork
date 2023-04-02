d0273da8be383db500502b461ecb094b799c064b
Ksenia Rozenbaum <kapellkaa@inbox.ru>
2023-04-02 23:33:25 +0700

    Домашняя работа "события и делегаты"

---
+++ HomeWork4/Handler1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    public class Handler1
+    {
+       public Handler1()
+        {
+            
+        }
+        public void Message() 
+        {
+                Console.WriteLine ("Уже достигнуто число 42!");
+        }
+    }
+}
